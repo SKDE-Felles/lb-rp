41ae3c46c363638022a32f5a0446a200641a0f5e
Are Edvardsen <biorakel@gmail.com>
2020-10-23 10:06:40 +0200

    provide certbot setup with new domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(mongr.no)
+domains=(mongr.no skde-resultater.no)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="biorakel@gmail.com" # Adding a valid address is strongly recommended
